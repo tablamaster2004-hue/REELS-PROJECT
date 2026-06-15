0477b2dfd72515660d5851dbf4b0bae5eb821bc7
tablamaster2004-hue <tablamaster2004@gmail.com>
2026-06-15 13:02:03 +0530

    server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,11 +1,12 @@
 const app = require("./src/app");
 const connectDB = require("./src/DATABASE/db");
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Startup failed:", error);
