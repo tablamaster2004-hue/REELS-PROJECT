1c7636832127076fe78c5600a3747212c4e2d040
tablamaster2004-hue <tablamaster2004@gmail.com>
2026-06-16 11:02:08 +0530

    Fix CORS for Vercel frontend

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -9,7 +9,7 @@ const foodPartnerRoutes = require("../src/routes/food_partner.routes")
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',"https://reels-project-eta.vercel.app"],
     credentials: true
 }))
 app.use(express.json());
