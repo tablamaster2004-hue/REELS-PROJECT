85f4ec9f1125a4c4ff0ea4be2abd1e87c2fddaa1
tablamaster2004-hue <tablamaster2004@gmail.com>
2026-06-18 16:59:24 +0530

    gmail smtp 587

--- BACKEND/src/services/email.service.js
+++ BACKEND/src/services/email.service.js
@@ -2,9 +2,11 @@ const nodemailer = require('nodemailer');
 const config = require('../config/config');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-        type: 'OAuth2',
+        type: "OAuth2",
         user: process.env.GOOGLE_USER,
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
